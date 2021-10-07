4dd80fc2cbac33ff94f948a8b98c2632321dee1d
Renato <renatoviei@gmail.com>
2021-10-06 22:20:08 -0300

    Criando projeto e classes básicas
    Foi criado o projeto .NET console app DIO.Bank, a classe básica Conta e a classe enum de TipoConta.

---
+++ DIO.Bank/DIO.Bank/Conta/Conta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Bank
+{
+    public class Conta
+    {
+    
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+
+    }
+}
---
+++ DIO.Bank/DIO.Bank/Enum/TipoConta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Bank
+{ 
+    public enum TipoConta
+    {
+        PessoFisica = 1,
+
+        PessoaJuridica = 2
+    }
+}
